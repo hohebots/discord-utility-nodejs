613d78dae8999f56b847787f89cb68037391e8f7
npas <97085060+npa5@users.noreply.github.com>
2023-06-14 08:28:51 +0200

    test
    asd

--- util/version.js
+++ util/version.js
@@ -2,14 +2,11 @@ const fs = require('fs');
 const log = require("./log")
 
 function load() {
-    try {
+    
         const data = fs.readFileSync('../version.json');
         const versionObject = JSON.parse(data);
         return versionObject.version;
-      } catch (error) {
-        log.error('Fehler beim Laden der Version:', error);
-        return null;
-    }
+      
 }
 
 module.exports = {
